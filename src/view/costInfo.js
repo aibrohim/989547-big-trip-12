20d80bcc9db64d135c05e099ace3d531b1967bff
Ibrohim7840 <Ibrohim7840@gmail.com>
2020-08-05 21:05:05 +0500

    task already done

---
+++ src/view/costInfo.js
@@ -0,0 +1,9 @@
+export const createCostInfo = () => {
+  return (
+    `
+    <p class="trip-info__cost">
+              Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
+            </p>
+    `
+  );
+};
