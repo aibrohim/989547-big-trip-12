9acb6755ec519275cad1857f78c791c16f47dea2
Ibrohim7840 <Ibrohim7840@gmail.com>
2020-09-10 11:05:45 +0500

    Errors have solved

--- src/main.js
+++ src/main.js
@@ -4,15 +4,7 @@ import CostInfoView from './view/costInfo.js';
 import MenuView from './view/menu.js';
 import FilterView from './view/filter.js';
 import BoardView from './presenter/board.js';
-import SortView from './view/sort.js';
-import EventEditorView from './view/eventEditor.js';
-import TripDaysListView from './view/tripDaysList.js';
-import TripDayView from './view/tripDay.js';
-import TripPointsListView from './view/tripPointsList.js';
-import TripPointView from './view/tripPoint.js';
-import NoPointView from './view/noPoint.js';
 import {generateTripPoint} from './mock/tripPoint.js';
-import {getSetDates} from "./utils/point.js";
 import {render, RenderPosition} from "./utils/render.js";
 
 const MAX_TRIPS = 15;
@@ -41,43 +33,10 @@ render(menuFilterFirstTitle, menuComponent, RenderPosition.AFTEREND);
 const filterComponent = new FilterView();
 render(menuFilterWrapper, filterComponent, RenderPosition.BEFOREEND);
 
-
-// SHO'TDAN BOSHLAB KOCHIRISH KERE
-
 const pageMain = document.querySelector(`.page-body__page-main`);
 const allEvents = pageMain.querySelector(`.trip-events`);
 
 const boardComponent = new BoardView(allEvents);
 
 boardComponent.init(tripPointsData);
-
-// if (tripPointsData.length === 0) {
-//   const noPoints = new NoPointView();
-//   render(allEvents, noPoints, RenderPosition.BEFOREEND);
-// } else {
-//   const sortComponent = new SortView();
-//   render(allEvents, sortComponent, RenderPosition.AFTERBEGIN);
-// }
-
-// if (tripPointsData.length > 0) {
-//   const tripDaysListComponent = new TripDaysListView();
-//   render(allEvents, tripDaysListComponent, RenderPosition.BEFOREEND);
-
-//   getSetDates(tripPointsData).forEach((date, index) => {
-//     const normalDate = new Date(date);
-//     const filtredData = tripPointsData.filter((dataItem) => {
-//       return dataItem.date.start.toDateString() === normalDate.toDateString();
-//     });
-
-//     const tripDayComponent = new TripDayView(normalDate, filtredData, index);
-//     render(tripDaysListComponent, tripDayComponent, RenderPosition.BEFOREEND);
-
-//     const tripPointsListComponent = new TripPointsListView();
-//     render(tripDayComponent, tripPointsListComponent, RenderPosition.BEFOREEND);
-
-//     filtredData.forEach((dataItem) => {
-//       renderPoint(tripPointsListComponent, dataItem);
-//     });
-//   });
-// }
 
