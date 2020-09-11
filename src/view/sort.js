0bbdf1792d2b3e7d012bc76272b2b441e4c9cf13
Ibrohim7840 <Ibrohim7840@gmail.com>
2020-09-11 15:46:11 +0500

    starting sort block

--- src/view/sort.js
+++ src/view/sort.js
@@ -1,4 +1,5 @@
 import {createElement} from "../utils.js";
+import {SortType} from "../consts.js"
 
 const createSort = () => {
   return (
@@ -7,12 +8,12 @@ const createSort = () => {
 
             <div class="trip-sort__item  trip-sort__item--event">
               <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" checked>
-              <label class="trip-sort__btn" for="sort-event">Event</label>
+              <label class="trip-sort__btn" for="sort-event" data-sort-type=${SortType.DEFAULT}>Event</label>
             </div>
 
             <div class="trip-sort__item  trip-sort__item--time">
               <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
-              <label class="trip-sort__btn" for="sort-time">
+              <label class="trip-sort__btn" for="sort-time" data-sort-type=${SortType.TIME}>
                 Time
                 <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
                   <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
@@ -22,7 +23,7 @@ const createSort = () => {
 
             <div class="trip-sort__item  trip-sort__item--price">
               <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
-              <label class="trip-sort__btn" for="sort-price">
+              <label class="trip-sort__btn" for="sort-price" data-sort-type=${SortType.PRICE}>
                 Price
                 <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
                   <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
